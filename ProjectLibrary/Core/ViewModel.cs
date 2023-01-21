8b0b590d81e398414ccda10907e300c31c39e385
kirN_Home <novitskiy.kir@gmail.com>
2023-01-21 20:41:35 +0300

    Добавлена общая структура MVVM
    - Добавлен класс ObservableObject в реализация уведомлений
    - Добавлен класс RelayCommand в реализация команд
    - Добавлен класс ViewModel - базовая реализация

---
+++ ProjectLibrary/Core/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary.Core
+{
+	public  class ViewModel : ObservableObject
+	{
+		private string _title;
+
+		public string Title
+		{
+			get { return _title; }
+			set => Set(value, ref _title);
+		}
+
+		public void Set<T>(T value, ref T filed, [CallerMemberName] string? propertyName = null)
+		{
+			if (Equals(value, filed))
+			{
+				return;
+			}
+
+			filed = value;
+			OnPropertyChanged(propertyName);
+		}
+
+	}
+}
