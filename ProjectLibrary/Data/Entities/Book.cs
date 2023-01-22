63bd003fd964ab1adcbb49567652eb397b4f0e9d
kirN_Home <novitskiy.kir@gmail.com>
2023-01-22 20:14:47 +0300

    Добавлены сущности Библиотеки

---
+++ ProjectLibrary/Data/Entities/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary.Data.Entities
+{
+	
+	public class Book
+	{
+		[Required]
+		public int Id { get; set; }
+		[Required]
+		public string Title { get; set; }
+		[Required]
+		public string Author { get; set; }
+		[Required]
+		public string ISBN { get; set; }
+		public string Annotation { get; set; }
+		public virtual Shelf Shelf { get; set; }
+		public virtual ICollection<Reader> Readers { get; set; }
+
+	}
+}
---
+++ ProjectLibrary/Data/Entities/Shelf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary.Data.Entities
+{
+	public class Shelf
+	{
+		[Required]
+		public int Id { get; set; }
+		[Required]
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public virtual ICollection<Book> Books { get; set; }
+	}
+}
