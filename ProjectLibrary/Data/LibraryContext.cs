e14b75e5d66e74ea375d15e63e960d964ffdacfc
kirN_Home <novitskiy.kir@gmail.com>
2023-01-22 20:15:10 +0300

    Добавлен контекст базы данных

---
+++ ProjectLibrary/Data/LibraryContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectLibrary.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary.Data
+{
+	internal class LibraryContext : DbContext
+	{
+		public DbSet<Book> Books { get; set; }
+		public DbSet<Shelf> Shelves { get; set; }
+		public DbSet<Reader> Readers { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder
+				.UseLazyLoadingProxies()
+				.UseSqlServer(@"Server=localhost\sqlexpress;Database=LibraryDB;Trusted_Connection=True;");
+		}
+	}
+}
