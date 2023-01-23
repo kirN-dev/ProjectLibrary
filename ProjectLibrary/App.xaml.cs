0d98217badfd2c42a230fb53a22d1004b4837c84
Silvan <Silvan@LAB44-00>
2023-01-23 16:35:07 +0300

    Добавлены регистрация ViewModel и View

--- ProjectLibrary/App.xaml.cs
+++ ProjectLibrary/App.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectLibrary.Core;
+using ProjectLibrary.MVVM.ViewModels;
+using ProjectLibrary.MVVM.Views;
+using ProjectLibrary.Services;
+using System;
 using System.Windows;
 
 namespace ProjectLibrary
@@ -13,5 +13,34 @@ namespace ProjectLibrary
 	/// </summary>
 	public partial class App : Application
 	{
-	}
+		private readonly ServiceProvider _serviceProvider;
+
+		public App()
+		{
+			IServiceCollection services = new ServiceCollection();
+
+			services.AddSingleton(provider => new MainWindow
+			{
+				DataContext = provider.GetRequiredService<MainViewModel>()
+			});
+
+			services.AddSingleton<MainViewModel>();
+			services.AddSingleton<BooksViewModel>();
+			services.AddSingleton<ReadersViewModel>();
+			services.AddSingleton<ShelvesViewModel>();
+
+			services.AddSingleton<INavigationService, NavigationService>();
+
+			services.AddSingleton<Func<Type, ViewModel>>(serviceProvider => viewModelType => (ViewModel)serviceProvider.GetRequiredService(viewModelType));
+
+			_serviceProvider = services.BuildServiceProvider();
+		}
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+			var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+			mainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
 }
