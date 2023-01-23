1d95f1cef8c1972f8f217c2913aa66581ca216a3
Silvan <Silvan@LAB44-00>
2023-01-23 14:42:58 +0300

    Добавлен сервис по смене View

---
+++ ProjectLibrary/Services/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿using ProjectLibrary.Core;
+
+namespace ProjectLibrary.Services
+{
+    public interface INavigationService
+    {
+        ViewModel CurrentView { get; }
+        void NavigateTo<TViewModel>() where TViewModel : ViewModel;
+    }
+}
---
+++ ProjectLibrary/Services/NavigationService.cs
@@ -0,0 +1,33 @@
+﻿using ProjectLibrary.Core;
+using System;
+
+namespace ProjectLibrary.Services
+{
+    public class NavigationService : Core.ObservableObject, INavigationService
+    {
+        private readonly Func<Type, ViewModel> _viewModelFactory;
+        private ViewModel _currentView;
+
+        public NavigationService(Func<Type, ViewModel> viewModelFactory)
+        {
+            _viewModelFactory= viewModelFactory;
+        }
+
+        public ViewModel CurrentView 
+        {
+            get => _currentView;
+            private set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void NavigateTo<TViewModel>() where TViewModel : ViewModel
+        {
+            ViewModel viewModel = _viewModelFactory(typeof(TViewModel));
+            CurrentView = viewModel;
+        }
+         
+    }
+}
